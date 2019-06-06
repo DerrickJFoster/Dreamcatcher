5e437922ca32aaebb8cd77a462078ee0cfe6b0ad
Derrick Foster <derrick.j.foster@gmail.com>
2019-06-05 20:49:13 -0600

    Set up dotenv and config variables for port and MONGODB_URI

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/products'
 const methodOverride = require('method-override');
 const dreamsController = require('./controllers/dreams');
-
+const dotenv = require('dotenv');
 app.use(methodOverride('_method'))
 
-mongoose.connect('mongodb://localhost:27017/products',{useNewUrlParser: true});
+mongoose.connect(MONGODB_URI,{useNewUrlParser: true});
 mongoose.connection.once('open', ()=>{
   console.log('connected to mongo');
 })
@@ -19,6 +21,6 @@ app.use(express.json())
 app.use('/', dreamsController)
 
 //listener
-app.listen(3000, ()=>{
-  console.log('listening on port:', 3000);
+app.listen(port, ()=>{
+  console.log('listening on port:', port);
 })
